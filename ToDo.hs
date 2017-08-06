cc1e5401d2ff412446caad688ca5a6876d2ae756
Giovanni Savini <giovanni.savini@stevini.it>
2017-08-06 15:25:26 +0200

    Added to-do deletion by number

--- ToDo.hs
+++ ToDo.hs
@@ -48,7 +48,7 @@ options =
             (\arg opt -> do doneToDo arg
                             exitWith ExitSuccess)
             "TO-DO")
-        "To-do done"
+        "To-do completed"
  
     , Option "h" ["help"]
         (NoArg
@@ -57,7 +57,7 @@ options =
                 let usage = "Usage: to-do -a \"to-do\""
                 hPutStrLn stderr (usageInfo usage options)
                 exitWith ExitSuccess))
-        "Show help"
+        "Show this help text"
     
     ]
 
@@ -74,29 +74,68 @@ addToDo :: [Char] -> IO ()
 addToDo todo = do
   putStrLn ("ADD " ++ todo)
   home <- getHomeDirectory
-  appendFile (home ++ "/.todo") (todo ++ "\n")
+  contents <- readFile (home ++ "/.todo")
+  evaluate (force contents)
+  let number = length (lines contents)
+  appendFile (home ++ "/.todo") (show (number + 1) ++ ") " ++ todo ++ "\n")
 
-doneToDo todo = do
+-- takes a todo or its number
+doneToDo :: String -> IO ()
+doneToDo input = do
   home <- getHomeDirectory
   contents <- readFile (home ++ "/.todo")
   evaluate (force contents)
   let todos = lines contents
-  let result = delete todo todos
-  -- clear file
-  writeFile (home ++ "/.todo") ""
-  rewrite result home
-  putStrLn ("DONE " ++ todo)
+  -- input deve essere dato nella forma "'1'". perche?
+  -- sistemato, vedi sotto
+  let res = deleteTodoByNumber (head input) todos
+
+  if res == todos
+    then putStrLn "no"
+    else do putStrLn "yes"
+            writeFile (home ++ "/.todo") ""
+            rewrite res home 1
+            putStrLn ("DONE " ++ input)
+
+
+-- deleteTodo :: String -> [String] -> [String]
+-- deleteTodo number todos = do
+--   if number == show (head (head todos))
+--     then return (delete (head todos) todos)
+--     else deleteTodo number (tail todos)
+
+deleteTodoByNumber :: Char -> [String] -> [String]
+deleteTodoByNumber number [] = []
+deleteTodoByNumber number (x:xs) = if number == head x -- non devo fare show (head x) perche mi da "'2'"
+                                   then deleteTodoByNumber number xs
+                                   else [x] ++ deleteTodoByNumber number xs
+
+-- deleteTodoByName name [] = return False
+-- deleteTodoByName name (x:xs) = if name == (drop 3 x)
+--                                then do delete x
+--                                        return True
+--                                else deleteTodoByName name xs
 
 -- it works, but it's ugly
-rewrite [] home = return ()
-rewrite (x:xs) home = do appendFile (home ++ "/.todo") (x ++ "\n")
-                         rewrite xs home
+rewrite [] home n = return ()
+rewrite (x:xs) home n = do appendFile (home ++ "/.todo") (show n ++ ") " ++ drop 3 x ++"\n")
+                           rewrite xs home (n + 1)
                  
 main :: IO ()
 main = do
+  
   args <- getArgs
   -- Parse options, getting a list of option actions
   let (actions, nonOptions, errors) = getOpt RequireOrder options args
+
+  home <- getHomeDirectory
+  check <- doesFileExist (home ++ "/.todo")
+  if check
+    -- perche devo fare cosi
+    -- IO da solo dei problemi
+    then return ()
+    else writeFile (home ++ "/.todo") ""
+    
   -- Here we thread startOptions through all supplied option actions
   opts <- foldl (>>=) (return startOptions) actions
   putStrLn "Not a valid command.\n-h is all you need"
